c8e0904b25783043c563991baee6f2f54cdc0b05
axelpeach <olkipri4@gmail.com>
2024-12-16 21:02:41 +0200

    Update main.py

--- kotomafik/bot/main.py
+++ kotomafik/bot/main.py
@@ -1,16 +1,16 @@
 import logging
 import os
-import asyncio
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 # Встановлення логування
-logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Команда /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("воркаю")
+    await update.message.reply_text(" воркаю ")
 
 # Команда /murr
 async def murr(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -19,27 +19,19 @@ async def murr(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 # Основна функція для запуску бота
 async def main():
+    # Токен для доступу до вашого бота, можна використовувати секрети
     TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-    # Створення бота
+    # Створення додатку та додавання команд
     application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # Реєстрація команд
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("murr", murr))
 
-    # Запуск бота через polling
+    # Запуск polling для обробки повідомлень
     await application.run_polling()
 
-# Перевірка та запуск event loop
-if __name__ == "__main__":
-    try:
-        # Якщо event loop уже існує, використовуємо його
-        loop = asyncio.get_event_loop()
-    except RuntimeError:
-        # Якщо event loop не існує, створюємо новий
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-
-    # Запускаємо основну функцію
-    loop.run_until_complete(main())
+# Запуск бота
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
