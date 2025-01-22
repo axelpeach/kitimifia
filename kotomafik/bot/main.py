b69523bacf13c5b4b351942ff863f0d387d11747
axelpeach <olkipri4@gmail.com>
2025-01-22 19:31:51 +0200

    Update main.py

--- kotomafik/bot/main.py
+++ kotomafik/bot/main.py
@@ -1,12 +1,13 @@
 import os
 import sqlite3
+import asyncio
 from flask import Flask, request, jsonify
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 # Завантаження змінних середовища
 TOKEN = os.getenv("TOKEN")
-MONOBANK_API = os.getenv("MONOBANK_API")
+MONOBANK_API = os.getenv("MONOBANK")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")  # URL вашого сервера
 JAR_LINK = "https://send.monobank.ua/jar/5yxJsnYG82"
 
